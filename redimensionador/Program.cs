44531f52a4e163c26611a9b9ae20de01eab1befd
MaikelTPeters <maikelpeters10@gmail.com>
2024-08-08 17:53:10 -0300

    Adicionar arquivos de projeto.

---
+++ redimensionador/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace redimensionador
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciando redimensionador");
+        }
+
+    }
+}
